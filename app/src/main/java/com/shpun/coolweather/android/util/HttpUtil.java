7645ebadcc5f591aa62617eeefe1fef2797ec818
shpunishment <shpunishment@gmail.com>
2018-02-22 20:01:15 +0800

    完成遍历省市县三级列表的功能.

---
+++ app/src/main/java/com/shpun/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.shpun.coolweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by shpun on 2018/2/22.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback); // enqueue 子线程
+    }
+
+}
